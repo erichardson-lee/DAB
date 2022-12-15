958cf579a1a1283ea3eb1039c9c60fb7f4b7ef79
Edward Richardson-lee <erichardson-lee@fairfields.co.uk>
2022-12-15 11:23:42 +0000

    Add Address Formatting to server/server.ts

--- server/server.ts
+++ server/server.ts
@@ -12,4 +12,16 @@ listen()
   .then(() => console.log("💤 Listener Closed"))
   .catch((err) => console.error("💥 Unhandled Error", err));
 
-console.log(`🚀 Server Started listening ${JSON.stringify(server.addr)}`);
+const formatAddr = (addr: Deno.Addr) => {
+  if (addr.transport === "tcp" || addr.transport === "udp") {
+    return `http://${addr.hostname}:${addr.port}`;
+  }
+
+  if (addr.transport === "unix" || addr.transport == "unixpacket") {
+    return `${addr.transport}://${addr.path}`;
+  }
+
+  throw new Error("Unknown Transport");
+};
+
+console.log(`🚀 Server Started listening on ${formatAddr(server.addr)}`);
