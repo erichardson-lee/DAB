441fad5933dd07c1ba01758cde52bc3517d5fd10
Edward Richardson-lee <erichardson-lee@fairfields.co.uk>
2022-12-15 17:02:29 +0000

    Simplify client for testing

--- client/main.ts
+++ client/main.ts
@@ -1,30 +1,16 @@
 import { DabClient } from "./dabclient.ts";
 
-const LOG_MESSAGES = false as const;
 const client = new DabClient({ debug: true });
 
 await client.connect({ broker: "ws://localhost:4142" });
 
-client.on("error", (_, e) => console.log("💥 Socket errored:", e));
-client.on("close", () => console.log("🔒 Socket closed"));
+console.log(
+  await client.send({
+    command: "register",
+    conversation: Math.floor(Math.random() * 1e12),
+    id: "739cc12e-3916-4e4e-bead-c75a777d5103",
+    name: "Fred",
+  }),
+);
 
-setInterval(() => {
-  client
-    .send({
-      command: "invoke",
-      conversation: Math.floor(Math.random() * 1e9),
-      id: "test",
-      payload: "AAAA",
-    })
-    .then((data) => {
-      LOG_MESSAGES && console.log("📫 Recieved:", data);
-    });
-}, 1000);
-
-const TIMEOUT = 10e3 as const;
-
-setTimeout(() => {
-  console.log(`⏰ Closing Socket (auto close after ${TIMEOUT}ms)`);
-  client.disconnect(1000, "Aight Imma Head Out ✌️");
-  Deno.exit(0);
-}, TIMEOUT);
+client.disconnect();
